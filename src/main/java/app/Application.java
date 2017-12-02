2143ffef55e7d87a1aaf42d06d16cad8cfdc684a
abr227 <christian.graumann@haw-hamburg.de>
2017-12-02 23:25:17 +0100

    adding Spring Boot for the heroservic

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -13,7 +13,7 @@ public class Application {
 
     public static void main(String[] args) throws UnknownHostException {
 
-        //SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
 
 
         System.out.println(java.net.InetAddress.getLocalHost().getHostAddress().toString());
