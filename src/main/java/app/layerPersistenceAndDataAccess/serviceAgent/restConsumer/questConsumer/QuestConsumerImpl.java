5ff0f08c8d505369728363d7dbd144a4847e7620
abr227 <christian.graumann@haw-hamburg.de>
2017-12-09 15:55:52 +0100

    adding JSONObject for deliverTaskpart and deliverTask

--- src/main/java/app/layerPersistenceAndDataAccess/serviceAgent/restConsumer/questConsumer/QuestConsumerImpl.java
+++ src/main/java/app/layerPersistenceAndDataAccess/serviceAgent/restConsumer/questConsumer/QuestConsumerImpl.java
@@ -109,19 +109,17 @@ public class QuestConsumerImpl implements IQuestConsumer {
     @Override
     public List<Delivery> deliverTask(Task task) throws UnexpectedResponseCodeException {
 
+        // creating the body with the task token
         JSONObject jsonToken = new JSONObject();
-
         jsonToken.put(Blackboard.getInstance().getUrl() + task.get_links().getSelf(), task.getToken());
 
         JSONObject jsonObject = new JSONObject();
-
         jsonObject.put("tokens", jsonToken);
 
 
         HTTPResponse response = this.httpCaller.post(
                 API.BLACKBOARD_QUESTS +"/" + task.getQuest() + "/deliveries",
                 Blackboard.getInstance().getUser().getUserToken(),
-                //"{ \"tokens\": { \"" + Blackboard.getInstance().getUrl() + task.get_links().getSelf() + "\": \"" + task.getToken() + "\" } }"
                 jsonObject.toString()
         );
 
@@ -136,37 +134,13 @@ public class QuestConsumerImpl implements IQuestConsumer {
     @Override
     public Visit deliverTaskPart(TaskPart taskpart) throws UnexpectedResponseCodeException {
 
+        // creating the body with the tokens from the steps
         List<String> tokenList = new ArrayList<String>();
         for (Step step : taskpart.getStepList()) {
             tokenList.add(step.getToken().getToken());
         }
 
-        JSONObject jsonObject = new JSONObject();
-
-        jsonObject.put("tokens", tokenList);
-
-        String tokens;
-
-        /*
-        String tokens = "{ \"tokens\":[";
-
-        for (Step step : taskpart.getStepList()) {
-            String token = "\"" + step.getToken().getToken() + "\", ";
-            tokens = tokens + token;
-        }
-
-        if (Blackboard.getInstance().getUser().getAssignmentDerliver() != null) {
-
-            tokens = tokens + Blackboard.getInstance().getUser().getAssignmentDerliver().getData();
-            tokens = tokens + "]}";
-
-        } else {
-            tokens = tokens.substring(0, tokens.length() - 2) + "]}";
-
-        }
-        */
-
-        tokens = jsonObject.toString();
+        String tokens = new JSONObject().put("tokens", tokenList).toString();
 
         HTTPResponse response = this.httpCaller.post("http://" + taskpart.getDeliverUri(), Blackboard.getInstance().getUser().getUserToken(), tokens);
 
