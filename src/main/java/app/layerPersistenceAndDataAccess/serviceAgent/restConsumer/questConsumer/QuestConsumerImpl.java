e753db39a7353ed841411b1f58d47a77993f50d7
abr227 <christian.graumann@haw-hamburg.de>
2017-12-09 01:30:55 +0100

    refactoring questConsumer

--- src/main/java/app/layerPersistenceAndDataAccess/serviceAgent/restConsumer/questConsumer/QuestConsumerImpl.java
+++ src/main/java/app/layerPersistenceAndDataAccess/serviceAgent/restConsumer/questConsumer/QuestConsumerImpl.java
@@ -42,134 +42,51 @@ public class QuestConsumerImpl implements IQuestConsumer {
     @Override
     public Quest getQuest(int index) throws UnexpectedResponseCodeException {
 
-        String token = Blackboard.getInstance().getUser().getUserToken();
+        HTTPResponse response = this.httpCaller.doGET(
+                Blackboard.getInstance().getUrl().toString() + "/blackboard/quests" + "/" + index,
+                Blackboard.getInstance().getUser().getUserToken()
+        );
 
+        return JSONUtil.getObject(response.getBody(), "object", Quest.class);
 
-        // Erstellen der Anfrage
-        HTTPRequest httpRequest =
-                new HTTPRequest(
-                        Blackboard.getInstance().getUrl().toString() + "/blackboard/quests" + "/" + index,
-                        EnumHTTPMethod.GET
-                );
-        httpRequest.setAuthorizationToken(token);
-
-        // Aufrufen des API´s Pfad
-        HTTPResponse response = this.httpCaller.call(httpRequest);
-
-        //System.out.println(response.toString());
-
-        // Antwort prüfen
-
-        if (response.getCode() != 200) {
-            ErrorCodeDTO errorCodeDTO = gson.fromJson(response.getBody(), ErrorCodeDTO.class);
-
-            throw new UnexpectedResponseCodeException(errorCodeDTO);
-
-        } else {
-            QuestDTO dto = gson.fromJson(response.getBody(), QuestDTO.class);
-
-            return dto.getObject();
-
-        }
     }
 
     @Override
     public Task getTask(int index) throws UnexpectedResponseCodeException {
-        String token = Blackboard.getInstance().getUser().getUserToken();
 
-        // /blackboard/quests/2/tasks
-        // Erstellen der Anfrage
-        HTTPRequest httpRequest =
-                new HTTPRequest(
-                        Blackboard.getInstance().getUrl().toString() + "/blackboard/tasks" + "/" + index,
-                        EnumHTTPMethod.GET
-                );
-        httpRequest.setAuthorizationToken(token);
+        HTTPResponse response = this.httpCaller.doGET(
+                Blackboard.getInstance().getUrl().toString() + "/blackboard/tasks" + "/" + index,
+                Blackboard.getInstance().getUser().getUserToken()
+        );
 
-        // Aufrufen des API´s Pfad
-        HTTPResponse response = this.httpCaller.call(httpRequest);
+        return JSONUtil.getObject(response.getBody(), "object", Task.class);
 
-        //System.out.println(response.toString());
-
-        // Antwort prüfen
-
-        if (response.getCode() != 200) {
-            ErrorCodeDTO errorCodeDTO = gson.fromJson(response.getBody(), ErrorCodeDTO.class);
-
-            throw new UnexpectedResponseCodeException(errorCodeDTO);
-
-        } else {
-            TaskDTO dto = gson.fromJson(response.getBody(), TaskDTO.class);
-
-            return dto.getObject();
-
-        }
     }
 
     @Override
     public Map lookAtTheMap(String location) throws UnexpectedResponseCodeException {
 
-        String token = Blackboard.getInstance().getUser().getUserToken();
+        HTTPResponse response = this.httpCaller.doGET(
+                Blackboard.getInstance().getUrl().toString() + "/map/" + location,
+                Blackboard.getInstance().getUser().getUserToken()
+        );
 
-        // Erstellen der Anfrage
-        HTTPRequest httpRequest =
-                new HTTPRequest(
-                        Blackboard.getInstance().getUrl().toString() + "/map/" + location,
-                        EnumHTTPMethod.GET
-                );
-        httpRequest.setAuthorizationToken(token);
 
-        // Aufrufen des API´s Pfad
-        HTTPResponse response = this.httpCaller.call(httpRequest);
+        return JSONUtil.getObject(response.getBody(), "object", Map.class);
 
-        //System.out.println(response.toString());
-
-        // Antwort prüfen
-
-        if (response.getCode() != 200) {
-            ErrorCodeDTO errorCodeDTO = gson.fromJson(response.getBody(), ErrorCodeDTO.class);
-
-            throw new UnexpectedResponseCodeException(errorCodeDTO);
-
-        } else {
-            MapDTO dto = gson.fromJson(response.getBody(), MapDTO.class);
-
-            return dto.getObject();
-
-        }
     }
 
     @Override
     public Visit visitHost(String ipPort, String ressource) throws UnexpectedResponseCodeException {
 
-        String token = Blackboard.getInstance().getUser().getUserToken();
+        HTTPResponse response = this.httpCaller.doGET(
+                "http://" + ipPort + ressource,
+                Blackboard.getInstance().getUser().getUserToken()
+        );
 
-        // Erstellen der Anfrage
-        HTTPRequest httpRequest =
-                new HTTPRequest(
-                        "http://" + ipPort + ressource,
-                        EnumHTTPMethod.GET
-                );
-        httpRequest.setAuthorizationToken(token);
 
-        // Aufrufen des API´s Pfad
-        HTTPResponse response = this.httpCaller.call(httpRequest);
+        return gson.fromJson(response.getBody(), Visit.class);
 
-        //System.out.println(response.toString());
-
-        // Antwort prüfen
-
-        if (response.getCode() != 200) {
-            ErrorCodeDTO errorCodeDTO = gson.fromJson(response.getBody(), ErrorCodeDTO.class);
-
-            throw new UnexpectedResponseCodeException(errorCodeDTO);
-
-        } else {
-            Visit visit = gson.fromJson(response.getBody(), Visit.class);
-
-            return visit;
-
-        }
     }
 
     @Override
@@ -209,39 +126,15 @@ public class QuestConsumerImpl implements IQuestConsumer {
     @Override
     public List<Delivery> deliverTask(Task task) throws UnexpectedResponseCodeException {
 
-        String authToken = Blackboard.getInstance().getUser().getUserToken();
 
-        //System.out.println(Blackboard.getInstance().getUrl().toString() + "/blackboard/quests/" + questId + "/deliveries");
+        HTTPResponse response = this.httpCaller.doPOST(
+                Blackboard.getInstance().getUrl().toString() + "/blackboard/quests/" + task.getQuest() + "/deliveries",
+                Blackboard.getInstance().getUser().getUserToken(),
+                "{ \"tokens\": { \"" + Blackboard.getInstance().getUrl() + task.get_links().getSelf() + "\": \"" + task.getToken() + "\" } }"
+        );
 
-        // Erstellen der Anfrage
-        HTTPRequest httpRequest =
-                new HTTPRequest(
-                        Blackboard.getInstance().getUrl().toString() + "/blackboard/quests/" + task.getQuest() + "/deliveries",
-                        EnumHTTPMethod.POST,
-                        "{ \"tokens\": { \"" + Blackboard.getInstance().getUrl() + task.get_links().getSelf() + "\": \"" + task.getToken() + "\" } }"
-                );
-        httpRequest.setAuthorizationToken(authToken);
+        return JSONUtil.getObjectList(response.getBody(), "object", Delivery.class);
 
-        //System.out.println(httpRequest.getBody().toString());
-
-        // Aufrufen des API´s Pfad
-        HTTPResponse response = this.httpCaller.call(httpRequest);
-
-        //System.out.println(response.toString());
-
-        // Antwort prüfen
-
-        if (response.getCode() != 201) {
-            ErrorCodeDTO errorCodeDTO = gson.fromJson(response.getBody(), ErrorCodeDTO.class);
-
-            throw new UnexpectedResponseCodeException(errorCodeDTO);
-
-        } else {
-            DeliverTaskDTO dto = gson.fromJson(response.getBody(), DeliverTaskDTO.class);
-
-            return dto.getObject();
-
-        }
     }
 
     @Override
@@ -300,248 +193,5 @@ public class QuestConsumerImpl implements IQuestConsumer {
         }
     }
 
-    private class QuestDTO {
 
-        // TODO - requires_tokens fehlt noch
-    /*
-    {
-  "object": {
-    "_links": {
-      "deliveries": "/blackboard/quests/3/deliveries",
-      "self": "/blackboard/quests/3",
-      "tasks": "/blackboard/quests/3/tasks"
-    },
-    "deliveries": [],
-    "description": "\nWhile you where away the Throneroom got attacked.\nWe were able to fight off the scum, but there are still a lot of wounded.\nPlease help to carry the wounded to the infirmary.",
-    "followups": [],
-    "id": 3,
-    "name": "Help the Wounded",
-    "prerequisites": [],
-    "requirements": "group",
-    "requires_tokens": {
-      "/blackboard/tasks/4": "Token:Carry a wounded person" // TODO - warum?
-    },
-    "reward": 1,
-    "tasks": [
-      "/blackboard/tasks/4"
-    ]
-  },
-  "status": "success"
-}
-
-     */
-
-        private Quest object;
-        private String status;
-
-        public QuestDTO(app.layerLogicAndService.cmpQuest.entity.Quest object, String status) {
-            this.object = object;
-            this.status = status;
-        }
-
-        public app.layerLogicAndService.cmpQuest.entity.Quest getObject() {
-            return object;
-        }
-
-        public void setObject(app.layerLogicAndService.cmpQuest.entity.Quest object) {
-            this.object = object;
-        }
-
-        public String getStatus() {
-            return status;
-        }
-
-        public void setStatus(String status) {
-            this.status = status;
-        }
-
-        @Override
-        public String toString() {
-            return "QuestDTO{" +
-                    "\nobject=" + object +
-                    ", \nstatus='" + status + '\'' +
-                    '}';
-        }
-
-
-    }
-
-
-    private class QuestsDTO {
-
-        List<Quest> objects;
-        String status;
-
-        public QuestsDTO(List<Quest> objects, String status) {
-            this.objects = objects;
-            this.status = status;
-        }
-
-        public List<Quest> getObjects() {
-            return objects;
-        }
-
-        public void setObjects(List<Quest> objects) {
-            this.objects = objects;
-        }
-
-        public String getStatus() {
-            return status;
-        }
-
-        public void setStatus(String status) {
-            this.status = status;
-        }
-
-        @Override
-        public String toString() {
-            return "QuestsDTO{" +
-                    "\nobjects=" + objects +
-                    ", \nstatus='" + status + '\'' +
-                    '}';
-        }
-
-    }
-
-
-    private class TaskDTO {
-
-        private Task object;
-        private String status;
-
-        public TaskDTO(Task object, String status) {
-            this.object = object;
-            this.status = status;
-        }
-
-        public Task getObject() {
-            return object;
-        }
-
-        public void setObject(Task object) {
-            this.object = object;
-        }
-
-        public String getStatus() {
-            return status;
-        }
-
-        public void setStatus(String status) {
-            this.status = status;
-        }
-
-        @Override
-        public String toString() {
-            return "TaskDTO{" +
-                    "\nobject=" + object +
-                    ", \nstatus='" + status + '\'' +
-                    '}';
-        }
-
-    }
-
-
-    private class MapDTO {
-
-        private Map object;
-        private String status;
-
-        public MapDTO(Map object, String status) {
-            this.object = object;
-            this.status = status;
-        }
-
-        public Map getObject() {
-            return object;
-        }
-
-        public void setObject(Map object) {
-            this.object = object;
-        }
-
-        public String getStatus() {
-            return status;
-        }
-
-        public void setStatus(String status) {
-            this.status = status;
-        }
-
-        @Override
-        public String toString() {
-            return "MapDTO{" +
-                    "\nobject=" + object +
-                    ", \nstatus='" + status + '\'' +
-                    '}';
-        }
-
-    }
-
-    private class DeliverTaskDTO {
-
-        /*
-
-{
-  "message": "Created Delivery",
-  "object": [
-    {
-      "deliverables": [
-        193
-      ],
-      "id": 434,
-      "quest": 1,
-      "timestamp": "2017-11-21T16:11:39.834529+00:00",
-      "user": "x2"
-    }
-  ],
-  "status": "success"
-}
-
-     */
-
-        private String message;
-        private List<Delivery> object;
-        private String status;
-
-        public DeliverTaskDTO(String message, List<Delivery> object, String status) {
-            this.message = message;
-            this.object = object;
-            this.status = status;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public List<Delivery> getObject() {
-            return object;
-        }
-
-        public void setObject(List<Delivery> object) {
-            this.object = object;
-        }
-
-        public String getStatus() {
-            return status;
-        }
-
-        public void setStatus(String status) {
-            this.status = status;
-        }
-
-        @Override
-        public String toString() {
-            return "DeliverTaskDTO{" +
-                    "\nmessage='" + message + '\'' +
-                    ", \nobject=" + object +
-                    ", \nstatus='" + status + '\'' +
-                    '}';
-        }
-
-
-    }
 }
